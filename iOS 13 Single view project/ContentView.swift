f446c82b8abf15154dc91581ca9b8d1e167a09e5
Alexmothe93 <alexmothe@hotmail.fr>
2024-08-12 03:42:08 -0700

    Initial Commit

---
+++ iOS 13 Single view project/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  iOS 13 Single view project
+//
+//  Created by Alexis Mothe on 12/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
